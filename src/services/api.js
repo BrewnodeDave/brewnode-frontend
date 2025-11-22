15c3355b9410cd44613875a0a812d9f1c0728ee9
BrewnodeDave <dave@brewnode.co.uk>
2025-11-22 22:33:36 +0000

    remove /api from requests

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '/',
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
