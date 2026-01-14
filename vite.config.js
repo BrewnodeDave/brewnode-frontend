3475b2602144a0302302c5d7fba32ca23291be90
BrewnodeDave <dave@brewnode.co.uk>
2026-01-14 12:07:52 +0000

    added fermenter switch

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3001,
     proxy: {
       // Proxy all API routes to the backend server
-      '^/(sensorStatus|brewdata|brewing|brewname|logs|mysql|restart|streamLog|batches|inventory|recipes|stream|fan|i2c|pumps|valve|valves|boil|chill|ferment|fill|k2f|k2m|m2k|mash|kettleTemp|heat|pump|glycol|kettleVolume|speedFactor|systemStatus|recirculate)': {
+      '^/(sensorStatus|brewdata|brewing|brewname|logs|mysql|restart|streamLog|batches|inventory|recipes|stream|fan|i2c|pumps|valve|valves|boil|chill|ferment|fill|k2f|k2m|m2k|mash|kettleTemp|heat|pump|glycol|kettleVolume|speedFactor|systemStatus|recirculate|activeFermenter)': {
         target: 'http://localhost:8080',
         changeOrigin: true
       }
